93d357643e86fa8d4dcec284680a497e2254b447
ronymaha <ronymaha@gmail.com>
2019-11-15 18:41:44 +0600

    upload all

--- elementor_widget_pro.php
+++ elementor_widget_pro.php
@@ -5,7 +5,7 @@ Plugin URI: http://elementor-widget.com
 Description: Elementor Widget Pro - Unlimited Widgets for Elementor Page Builder, with html/css/js widget creator and editor.
 Author: Ruben Romero
 Version: 1.0.1
-Author URI: http://authorurl.com
+Author URI: http://authorurl.com/
 */
 
 
